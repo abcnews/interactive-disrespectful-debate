140280e39034f730a21a8b477a23cfbb422f94c4
Joshua Byrd <phocks@gmail.com>
2023-01-17 11:47:41 +1000

    Add babel preset

--- src/index.js
+++ src/index.js
@@ -1,33 +1,40 @@
-const { h, render } = require('preact');
-const { loadSection } = require('./loader');
+const { h, render } = require("preact");
+const { loadSection } = require("./loader");
 
-const root = document.querySelector('[data-interactive-disrespectful-debate-root]');
+const root = document.querySelector("[data-interactive-disrespectful-debate-root]");
 
 function init() {
+  console.log("hello");
   const section = loadSection();
 
-  const App = require('./components/App');
-  render(<App panels={section.panels} />, section.mountNode, section.mountNode.firstChild);
+  const App = require("./components/App");
+  render(
+    <App panels={section.panels} />,
+    section.mountNode,
+    section.mountNode.firstChild
+  );
 }
 
 if (module.hot) {
-  module.hot.accept('./components/App', () => {
+  module.hot.accept("./components/App", () => {
     try {
       init();
     } catch (err) {
-      const ErrorBox = require('./components/ErrorBox');
+      const ErrorBox = require("./components/ErrorBox");
       render(<ErrorBox error={err} />, root, root.firstChild);
     }
   });
 }
 
-if (process.env.NODE_ENV === 'development') {
-  require('preact/devtools');
-  console.debug(`[interactive-disrespectful-debate] public path: ${__webpack_public_path__}`);
+if (process.env.NODE_ENV === "development") {
+  require("preact/devtools");
+  console.debug(
+    `[interactive-disrespectful-debate] public path: ${__webpack_public_path__}`
+  );
 }
 
 if (window.__ODYSSEY__) {
   init();
 } else {
-  window.addEventListener('odyssey:api', init);
+  window.addEventListener("odyssey:api", init);
 }
